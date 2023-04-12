6e543e56f5602c744accccabe887800e053c7e14
“Kivi” <wbc583741544@gmail.com>
2023-04-12 18:29:59 +0800

    added multer

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ['bcrypt', 'spark-md5'],
+    serverComponentsExternalPackages: ['bcrypt', 'spark-md5, multer'],
   },
   images: {
     domains: [
